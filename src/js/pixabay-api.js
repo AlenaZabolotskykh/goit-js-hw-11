02c2dcfd60ed6570db643510e84501f35dba085a
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-07-08 16:44:14 +0300

    final

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,8 @@
-export function getPicturesByQuery(query) {
-  const KEY = '44791676-0ec883c6e453828f8c76360bc';
+const loader = document.querySelector('.loader')
 
+export function getPicturesByQuery(query) {
+  loader.style.display = 'inline-block';
+const KEY = '44791676-0ec883c6e453828f8c76360bc';
   return fetch(
     `https://pixabay.com/api/?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
   ).then((res) => {
@@ -14,41 +16,3 @@ export function getPicturesByQuery(query) {
 }
    
 
-
-// function handlerForm (evt){
-//     evt.preventDefault()
-//     gallery.innerHTML = "";
-//    const inputValue = form.elements.search.value;
-// getPicturesByQuery(inputValue)
-// .then(data => renderImage(data.hits))
-// .catch((err)=>console.log(err))
-// }
-
-// function renderImage(images) {
-//     if (images.length === 0) {
-//         iziToast.show({
-//             message: 'Sorry, there are no images matching your search query. Please try again!'
-//           });
-//         gallery.innerHTML = "";
-//       }
-//       else {
-//     const listItems = images
-//     .map(({largeImageURL, webformatURL, tags, likes, views, comments, downloads}) => `<li class="list">
-//   <a href="${largeImageURL}">
-//     <img src='${webformatURL}' alt='${tags}'/>
-//     <div class="item-wrap">
-//     <h2 class="item-text">Like<p>${likes}</p></h2>
-//     <h2 class="item-text">Views<p>${views}</p></h2>
-//     <h2 class="item-text">Comments<p>${comments}</p></h2>
-//     <h2 class="item-text">Downloads<p>${downloads}</p></h2>
-//   </div>
-//   </li>`)
-//     .join("");
-
-//   gallery.innerHTML = listItems;
-//   lightbox = new SimpleLightbox('.gallery a', {docClose: true,
-//     close: true,
-//   });
-//   lightbox.refresh();
-//   }}
-  
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("form")
 const gallery = document.querySelector('.gallery')
+// const loader = document.querySelector('.loader')
 
 form.addEventListener('submit', handlerForm)
 
@@ -12,6 +13,8 @@ function handlerForm (evt){
     gallery.innerHTML = "";
    const inputValue = form.elements.search.value;
 getPicturesByQuery(inputValue)
-.then(data => renderImage(data.hits))
+.then(data => {renderImage(data.hits),
+    loader.style.display = 'none'
+})
 .catch((err)=>console.log(err))
 }
